208ae2296131f7cadf8cfea83a93f09b96582c03
isalzufari <isalzufari@gmail.com>
2022-07-22 07:24:30 +0700

    Updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 
 export const FooterComponent = () => {
   return <footer className={styles.footer}>Quality Air - 2022</footer>;
